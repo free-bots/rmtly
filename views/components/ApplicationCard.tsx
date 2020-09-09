7dc9c7918cb268b438a7006f4d1b1b0d38679b24
free-bots <free-bots.to@protonmail.com>
2020-09-09 16:02:21 +0200

    card styling

--- views/components/ApplicationCard.tsx
+++ views/components/ApplicationCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {ActivityIndicator, Dimensions, PixelRatio, Text, View} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 
 export const ApplicationCard = ({name, icon}: {name: string; icon: string}) => {
   const loading = true;
   return (
     <View
       style={{
+        position: 'relative',
         backgroundColor: '#fff',
         borderRadius: 10,
         elevation: 10,
@@ -17,8 +18,23 @@ export const ApplicationCard = ({name, icon}: {name: string; icon: string}) => {
         // alignItems: 'center',
       }}>
       {/*<Image source={undefined}/>*/}
-      <Text>{name}</Text>
-      {loading && <ActivityIndicator size={'large'} color={'green'} />}
+      <Text>Name:{name}</Text>
+      {loading && (
+        <ActivityIndicator
+          size={'large'}
+          color={'#7e23e8'}
+          style={{
+            position: 'absolute',
+            margin: 'auto',
+            height: '50%',
+            width: '50%',
+            top: 33,
+            left: 33,
+            right: 33,
+            bottom: 33,
+          }}
+        />
+      )}
     </View>
   );
 };
