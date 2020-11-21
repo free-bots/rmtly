73b188b4c68a7765a3f0e9271ae2b4aecfca0f1e
free-bots <free-bots.to@protonmail.com>
2020-11-21 11:12:28 +0100

    added categories route

--- App.tsx
+++ App.tsx
@@ -15,18 +15,27 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from './views/pages/Home';
 import {Connection} from './views/pages/settings/Connection';
+import {Categories} from './views/pages/Categories';
+import {DefaultTheme, DarkTheme} from '@react-navigation/native';
+import {useColorScheme} from 'react-native';
 
 declare const global: {HermesInternal: null | {}};
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
+  const scheme = useColorScheme();
+
   return (
     <ThemeContextProvider>
       <ApplicationsContextProvider>
-        <NavigationContainer>
-          <Drawer.Navigator initialRouteName="Home">
+        <NavigationContainer
+          theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Drawer.Navigator
+            initialRouteName="Home"
+            detachInactiveScreens={true}>
             <Drawer.Screen name="Applications" component={Home} />
+            <Drawer.Screen name="Categories" component={Categories} />
             <Drawer.Screen name="Connection" component={Connection} />
           </Drawer.Navigator>
         </NavigationContainer>
---
+++ views/pages/Categories.tsx
@@ -0,0 +1,13 @@
+import {SafeAreaView, StatusBar, View} from 'react-native';
+import React from 'react';
+
+export const Categories = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={'#7e23e8'} />
+      <SafeAreaView>
+        <View />
+      </SafeAreaView>
+    </>
+  );
+};
